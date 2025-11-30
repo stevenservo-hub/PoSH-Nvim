bb41f0555cf723469fe3e6397430d76ca6ccb4f8
Steven Spring <82784665+stevenservo-hub@users.noreply.github.com>
2025-11-29 19:30:53 -0800

    Disable diagnostics in neo-tree configuration

--- init.lua
+++ init.lua
@@ -90,6 +90,7 @@ require("lazy").setup({
     },
     config = function()
         require("neo-tree").setup({
+            enable_diagnostics = false,
             close_if_last_window = true,
             filesystem = {
                 hijack_netrw_behavior = "open_default",
